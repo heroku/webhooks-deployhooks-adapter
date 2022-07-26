6e3a9a0e8da6a9e768493b676abb23d51636dd84
srisaro <srisaro@heroku.com>
2022-07-26 11:59:55 -0300

    Reorder things to improve readability

--- app/hook.rb
+++ app/hook.rb
@@ -7,7 +7,10 @@ class HookAdapter < Sinatra::Base
   post '/' do
     verify_authorization!
     verify_message_digest!
-    invoke_http_hook(body: deployhooks_formated_body) if http_hook_configured && release_finished
+    verify_release_finished!
+
+    invoke_http_hook(body: deployhooks_formated_body) if http_hook_configured
+
     204
   rescue Excon::Error => e
     puts "An error occured while sending the request: #{e}"
@@ -15,20 +18,6 @@ class HookAdapter < Sinatra::Base
   end
 
   private
-
-  attr_reader :webhook_payload
-
-  def webhook_payload
-    @webhook_payload ||= parse_body
-  end
-
-  def parse_body
-    request.body.rewind
-    JSON.parse(request.body.read)
-  rescue StandardError => e
-    puts "An error occured while parsing the request: #{e}"
-    {}
-  end
 
   def verify_authorization!
     return unless authorization_enabled
@@ -61,11 +50,34 @@ class HookAdapter < Sinatra::Base
     heroku_hmac && Rack::Utils.secure_compare(calculated_hmac, heroku_hmac)
   end
 
+  def verify_release_finished!
+    halt 204 unless release_finished
+  end
+
   def release_finished
     status = webhook_payload.dig('data', 'status')
     action = webhook_payload['action']
     is_current = webhook_payload.dig('data', 'current')
     status.eql?('succeeded') && action.eql?('update') && is_current
+  end
+
+  def invoke_http_hook(body: '')
+    headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
+    Excon.new(http_hook_uri).request(
+      method: :post,
+      expects: [200, 204],
+      body: body.to_json,
+      headers: headers
+    )
+  end
+
+  def http_hook_uri
+    uri = URI.parse(ENV['HTTP_ENDPOINT'])
+    variable = /{{(\w+)}}/
+    uri.to_s.gsub(variable) do |match_string|
+      matchdata = match_string.match(variable)
+      Addressable::URI.encode_component(deployhooks_formated_body[matchdata[1]], Addressable::URI::CharacterClasses::PATH)
+    end
   end
 
   # Formats webhooks' payload to resemble deployhooks behavior.
@@ -82,26 +94,21 @@ class HookAdapter < Sinatra::Base
     }
   end
 
-  def invoke_http_hook(body: '')
-    headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
-    Excon.new(http_hook_uri).request(
-      method: :post,
-      expects: [200, 204],
-      body: body.to_json,
-      headers: headers
-    )
-  end
-
   def http_hook_configured
     !ENV['HTTP_ENDPOINT'].nil?
   end
 
-  def http_hook_uri
-    uri = URI.parse(ENV['HTTP_ENDPOINT'])
-    variable = /{{(\w+)}}/
-    uri.to_s.gsub(variable) do |match_string|
-      matchdata = match_string.match(variable)
-      Addressable::URI.encode_component(deployhooks_formated_body[matchdata[1]], Addressable::URI::CharacterClasses::PATH)
-    end
+  attr_reader :webhook_payload
+
+  def webhook_payload
+    @webhook_payload ||= parse_body
+  end
+
+  def parse_body
+    request.body.rewind
+    JSON.parse(request.body.read)
+  rescue StandardError => e
+    puts "An error occured while parsing the request: #{e}"
+    {}
   end
 end
